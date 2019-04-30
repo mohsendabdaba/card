7fe456fcbdf9d1924692e7c73f001e40ae584882
mohsendabdaba <dabdabmohsen@gmail.com>
2019-04-30 16:47:54 +0200

    card v1.0.0

---
+++ src/card.js
@@ -0,0 +1,44 @@
+import React from 'react'
+
+
+
+
+const card = () =>{
+    return(
+
+        <div className="card">
+        
+        
+        <h1>CREDIT CARD</h1>
+        <img className="image2"src="https://i.imgur.com/jaxefe4.png" alt=""></img>
+         
+        <h2> 7253  3256 7895 1245 </h2>
+
+        <img className="image3"src="https://q7h8y4i2.stackpathcdn.com/wp-content/uploads/2013/12/visa-mastercard-logo.jpg" alt=""></img>
+        <div class="low-card">
+           <div class="number">
+              <h1>5422</h1>
+              <h1>CARDHOLDER</h1>
+           </div>
+           <div class="valid">
+              <h4>VALID</h4>
+              <h5>THRU</h5>
+           </div>
+           <div class="describe">
+              <h6>MONTH/YEAR</h6>
+              <h3>11/50</h3>
+           </div>
+           
+    
+        </div>
+       
+        </div>
+    
+    
+    )
+
+}
+
+
+
+export default card
